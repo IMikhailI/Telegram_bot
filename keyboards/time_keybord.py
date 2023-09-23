39019eb6be0f0af65f0f0013cf6f534fa22611f6
IMikhailI <Dobrodomov_mikhail@mail.ru>
2023-09-23 11:05:07 +0500

    клавиатура с выбором времени для записи

--- keyboards/time_keybord.py
+++ keyboards/time_keybord.py
@@ -1,16 +1,46 @@
 from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
+import sqlite3 as sq
+
+import os
+
+from Telegram_bot.database.path import get_script_dir
 
 
-def time_keyboards():
-    info = ["время 1", "время 2", "время 3", "время 4", "время 5", "время 6", "Отмена"]
+# def get_script_dir():
+#     abs_path = path.abspath(__file__)  # полный путь к файлу скрипта
+#     return path.dirname(abs_path)
+
+
+def time_keyboards(doc):
+
+    conn = sq.connect(os.path.join(get_script_dir(), 'test_base.db'))
+    cur = conn.cursor()
+
+    cur.execute(f"""SELECT e.start_date
+                    FROM EVENT e
+                    JOIN WORKER w 
+                    ON e.worker_id = w.id
+                    WHERE sur_name||" "||first_name||" "||patr_name = '{doc}' AND e.client_data IS NULL """)
+
+    users = cur.fetchall()
+
+    info = []
+    for el in users:
+        info += el[0::]
+
+    cur.close()
+    conn.close()
+
+    info.append("Отмена")
+    print(info)
 
     # Заполняем список списками с кнопками
     keyboard: list[list[KeyboardButton]] = [
         [KeyboardButton(text=i)] for i in info]
 
     # Создаем объект клавиатуры, добавляя в него список списков с кнопками
-    speciality_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
+    time_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
         keyboard=keyboard,
-        resize_keyboard=True)
+        resize_keyboard=True, one_time_keyboard=True)
 
-    return speciality_keyboard
+    return time_keyboard
