ebfa072ad468952b049d3a497f8defae0cb7701c
IMikhailI <Dobrodomov_mikhail@mail.ru>
2023-09-23 11:11:15 +0500

    клавиатура меню

--- keyboards/main_keyboard.py
+++ keyboards/main_keyboard.py
@@ -2,10 +2,10 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 # Создаем объекты главных кнопок
 button_1: KeyboardButton = KeyboardButton(text='Выбрать специальность врача')
-button_2: KeyboardButton = KeyboardButton(text='Посмотреть мою запись')  # возможно не понадобятся
-button_3: KeyboardButton = KeyboardButton(text='Отменить запись')  # возможно не понадобятся
+# button_2: KeyboardButton = KeyboardButton(text='Посмотреть мою запись')  # возможно не понадобятся
+# button_3: KeyboardButton = KeyboardButton(text='Отменить запись')  # возможно не понадобятся
 
 # Создаем объект клавиатуры, добавляя в него кнопки
 main_keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
-    keyboard=[[button_1, button_2, button_3]],
+    keyboard=[[button_1]],
     resize_keyboard=True)
